128f6acb48dc0cfaa25143126c9013486594d511
Stephen Lewis <stephen@experiencehq.net>
2013-07-12 10:16:41 +0100

    Delete obsolete 'create user' manifest.
    Two reasons:
    1. We might as well do this as part of the GRANT operation;
    2. The user's password would not be set correctly when using CREATE USER.

--- manifests/user/create.pp
+++
@@ -1,9 +0,0 @@
-define mysql::user::create ($user = $title, $password, $host) {
-  exec { "mysql::user::create_${user}_${host}":
-    command => "mysql -uroot -p${mysql::root_password} -e \"CREATE USER '${user}'@'${host}' IDENTIFIED BY '${password}'\"",
-    onlyif  => "mysql -uroot -p${mysql::root_password} -e \"SELECT EXISTS(SELECT `user` FROM `mysql`.`user` WHERE `user` = '${user}' AND `host` = '${host}');\"",
-    path    => $mysql::bin,
-    require => Exec['mysql::set_root_password'],
-  }
-}
-
