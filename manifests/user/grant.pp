5f2cf56aa5dac64586430f6cec129c6f22184d6d
Stephen Lewis <stephen@experiencehq.net>
2013-07-12 10:15:35 +0100

    Roll user creation into 'GRANT' manifest.

--- manifests/user/grant.pp
+++ manifests/user/grant.pp
@@ -1,6 +1,6 @@
-define mysql::user::grant ($user = $title, $host, $database, $table = '*', $privileges = 'ALL PRIVILEGES') {
+define mysql::user::grant ($user = $title, $host, $password, $database, $table = '*', $privileges = 'ALL PRIVILEGES') {
   exec { "mysql::user::grant_${user}_${host}_${database}_${table}_${privileges}":
-    command => "mysql -uroot -p${mysql::root_password} -e \"GRANT ${privileges} ON ${database}.${table} TO '${user}'@'${host}'; FLUSH PRIVILEGES;\"",
+    command => "mysql -uroot -p${mysql::root_password} -e \"GRANT ${privileges} ON ${database}.${table} TO '${user}'@'${host}' IDENTIFIED BY '${password}'; FLUSH PRIVILEGES;\"",
     path    => $mysql::bin,
     require => [
       Exec['mysql::set_root_password'],
